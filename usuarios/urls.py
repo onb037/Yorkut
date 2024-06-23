966baf3091de6fbcf2f8f8a0b356b2899b0b1c0e
Jeanaraga <jeanjesuspedrobook@gmail.com>
2024-06-22 22:34:07 -0300

    update criação postar
    Criei e já coloquei na view a página para fazer uma postagem.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from usuarios.views import login, cadastro
+from usuarios.views import login, cadastro, postar
 urlpatterns = [
     path('login', login, name='login'),
     path('cadastro', cadastro, name='cadastro'),
+    path('postar', postar, name='postar'),
 ]
