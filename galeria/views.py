91b803638562ac068337dfad143eb6990a921163
NTales <128847014+nTales129@users.noreply.github.com>
2024-06-24 12:19:53 -0300

    postagems

--- galeria/views.py
+++ galeria/views.py
@@ -1,11 +1,14 @@
+#views
+
 from django.shortcuts import render
+
+from galeria.models import Post
 
 
 def  index(request):
     return render(request, 'galeria/index.html')
 
 def cadastro(request):
-    return render(request, 'galeria/card.html')
-    
-
-# Create your views here.
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'galeria/card.html', context)
