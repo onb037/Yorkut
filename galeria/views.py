c1200592b0add3d6c229c9bdd0caeb44327c1fc0
NTales <128847014+nTales129@users.noreply.github.com>
2024-06-24 14:37:13 -0300

    logout

--- galeria/views.py
+++ galeria/views.py
@@ -9,6 +9,6 @@ def  index(request):
     return render(request, 'galeria/index.html')
 
 def cadastro(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(privacidade=True)
     context = {'posts': posts}
     return render(request, 'galeria/card.html', context)
