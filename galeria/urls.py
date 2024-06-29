9983afa90cf9c7d5639c26373442107c6838004a
Jeanaraga <jeanjesuspedrobook@gmail.com>
2024-06-29 02:12:03 -0300

    feat: página amigos

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from galeria.views import index, card, postar, editar_post, apagar_post, verpost, perfil
+from galeria.views import index, card, postar, editar_post, apagar_post, verpost, perfil, amigos
 
 urlpatterns = [
     path('', index),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('verpost', verpost, name='verpost'),
     path('editar-post', editar_post, name='editar_post'),
     path('apagar-post', apagar_post, name='apagar_post'),
-    path('perfil', perfil, name='perfil')  
+    path('perfil', perfil, name='perfil'),
+    path('amigos', amigos, name='amigos')
 ]
